716a8daed623ec461d3082a5bf1038e27da44c9c
vistepanov <vistepanov@spasibosb.ru>
2024-10-29 13:55:49 +0300

    Доработка приложения согласно заданию спринта 6

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,8 @@ public class Main {
         System.out.println("Поехали!");
 
         TaskManager taskManager = Managers.getDefault();
+
+        // Дополнительное задание
         Task task1 = new Task("task1", "description2", TaskStatus.NEW);
         Task task2 = new Task("task2", "description2", TaskStatus.NEW);
         taskManager.addTask(task1);
@@ -21,49 +23,39 @@ public class Main {
 
         Subtask subtask1 = new Subtask("subtask1", "descr1", TaskStatus.NEW, epic1.getId());
         Subtask subtask2 = new Subtask("subtask2", "descriprion2", TaskStatus.NEW, epic1.getId());
+        Subtask subtask3 = new Subtask("subtask3", "descriprion3", TaskStatus.NEW, epic1.getId());
         taskManager.addSubtask(subtask1);
         taskManager.addSubtask(subtask2);
+        taskManager.addSubtask(subtask3);
 
         Epic epic2 = new Epic("epicTitle2", "epicDescription2");
         taskManager.addEpic(epic2);
 
-        Subtask subtask3 = new Subtask("subtask3", "descr3", TaskStatus.NEW, epic2.getId());
-        taskManager.addSubtask(subtask3);
+        taskManager.getSubtaskById(subtask1.getId());
+        taskManager.getTaskById(task1.getId());
+        taskManager.getEpicById(epic1.getId());
+        taskManager.getSubtaskById(subtask2.getId());
+        taskManager.getEpicById(epic2.getId());
+        taskManager.getSubtaskById(subtask3.getId());
+        taskManager.getTaskById(task2.getId());
+        printAllTasks(taskManager);
+
+        taskManager.getEpicById(epic1.getId());
+        printAllTasks(taskManager);
+
+        taskManager.getSubtaskById(subtask1.getId());
+        taskManager.getSubtaskById(subtask3.getId());
+        taskManager.getTaskById(task1.getId());
 
         printAllTasks(taskManager);
 
-        Task updateTask1 = taskManager.getTaskById(task1.getId());
-        Task changeTask1 = new Task(updateTask1.getId(), updateTask1.getTitle(), updateTask1.getDescription(),
-                TaskStatus.DONE);
-        taskManager.updateTask(changeTask1);
-        Task updateTask2 = taskManager.getTaskById(task2.getId());
-        Task changeTask2 = new Task(updateTask2.getId(), updateTask2.getTitle(), updateTask2.getDescription(),
-                TaskStatus.IN_PROGRESS);
-        taskManager.updateTask(changeTask2);
-        printAllTasks(taskManager);
-
-        Subtask updateSubtask1 = taskManager.getSubtaskById(subtask1.getId());
-        Subtask updateSubtask2 = taskManager.getSubtaskById(subtask2.getId());
-        Subtask changeSubtask1 = new Subtask(updateSubtask1.getId(), updateSubtask1.getTitle(),
-                updateSubtask1.getDescription(), TaskStatus.IN_PROGRESS, epic2.getId());
-        Subtask changeSubtask2 = new Subtask(updateSubtask2.getId(), updateSubtask2.getTitle(),
-                updateSubtask2.getDescription(), TaskStatus.DONE, updateSubtask2.getEpicId());
-        taskManager.updateSubtask(changeSubtask1);
-        taskManager.updateSubtask(changeSubtask2);
-        printAllTasks(taskManager);
-
-        Epic updateEpic2 = taskManager.getEpicById(epic2.getId());
-        Epic changedEpic2 = new Epic(updateEpic2.getId(), "epicTitle2changed", updateEpic2.getDescription(),
-                updateEpic2.getSubtasks());
-        taskManager.updateEpic(changedEpic2);
-        taskManager.removeSubtasks();
-        taskManager.removeTaskById(task1.getId());
         taskManager.removeSubtaskById(subtask1.getId());
-        taskManager.removeEpicById(epic2.getId());
-        taskManager.removeEpics();
-        taskManager.removeTasks();
-        System.out.println(taskManager.getAllTasks());
         printAllTasks(taskManager);
+
+        taskManager.removeEpicById(epic1.getId());
+        printAllTasks(taskManager);
+
+
     }
 
     private static void printAllTasks(TaskManager manager) {
--- src/taskhistory/InMemoryHistoryManager.java
+++ src/taskhistory/InMemoryHistoryManager.java
@@ -3,26 +3,93 @@ package taskhistory;
 import task.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class InMemoryHistoryManager implements HistoryManager{
+public class InMemoryHistoryManager implements HistoryManager {
 
-    private final List<Task> history;
+    static class Node<Task> {
+        public Task data;
+        public Node<Task> next;
+        public Node<Task> prev;
+
+        public Node(Task data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    private Node<Task> first;
+    private Node<Task> last;
+
+    private final HashMap<Integer, Node<Task>> historyHashMap;
 
     public InMemoryHistoryManager() {
-        this.history = new ArrayList<>();
+        this.first = null;
+        this.last = null;
+        this.historyHashMap = new HashMap<>();
     }
 
     @Override
     public void add(Task task) {
-        if(history.size() == 10) {
-            history.removeFirst();
+        remove(task.getId());
+
+        Node<Task> newNode = new Node<>(task);
+        if (this.first == null) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            newNode.prev = this.last;
+            this.last.next = newNode;
+            this.last = newNode;
         }
-        history.add(task);
+        historyHashMap.put(task.getId(), newNode);
+    }
+
+    @Override
+    public void remove(int id) {
+        Node<Task> removeNode = historyHashMap.get(id);
+        if(removeNode != null) {
+            if(this.first == removeNode) {
+                Node<Task> next = this.first.next;
+                if(next != null) {
+                    next.prev = null;
+                    this.first = next;
+                } else {
+                    this.first = null;
+                }
+            }
+            if(this.last == removeNode) {
+                Node<Task> prev = this.last.prev;
+                if(prev != null) {
+                    prev.next = null;
+                    this.last = prev;
+                } else {
+                    this.last = null;
+                }
+            } else {
+                Node<Task> next = removeNode.next;
+                Node<Task> prev = removeNode.prev;
+                if(next != null && prev != null) {
+                    next.prev = prev;
+                    prev.next = next;
+                }
+            }
+            historyHashMap.remove(id);
+        }
     }
 
     @Override
     public List<Task> getHistory() {
+        ArrayList<Task> history = new ArrayList<>();
+        if(!historyHashMap.isEmpty()) {
+            Node<Task> iteratedTaskNode = this.first;
+            do {
+                history.add(iteratedTaskNode.data);
+                iteratedTaskNode = iteratedTaskNode.next;
+            } while (iteratedTaskNode != null);
+        }
         return history;
     }
 }
